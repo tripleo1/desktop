31a0e8033d9a31742069921541e8774a6154c87b
Brendan Forster <github@brendanforster.com>
2021-11-04 16:38:42 -0300

    stub stats store setup so that it doesn't automatically make network calls in tests

--- app/test/setup-test-framework.ts
+++ app/test/setup-test-framework.ts
@@ -3,3 +3,9 @@ jest.setTimeout(10000)
 
 import 'jest-extended'
 import 'jest-localstorage-mock'
+
+// by default we set this value in local storage so that the StatsStore doesn't
+// call out to the stats endpoint during tests and result in open handles at
+// the end of the test run
+localStorage.setItem('has-sent-stats-opt-in-ping', 'true')
+localStorage.setItem('stats-opt-out', 'true')
